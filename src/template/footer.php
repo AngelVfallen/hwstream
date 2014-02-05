6520e46a3c2345b92c9668d7c1baccb7b138bec2
asleepwalker <mail@asleepwalker.ru>
2014-02-05 11:23:46 +0200

    Important comments now highlighting their lessons

--- src/template/footer.php
+++ src/template/footer.php
@@ -1,5 +1,7 @@
 		<div id="edgeLeft" class="displayEdges" style="display: none"><div id="stepPrev"></div></div>
 		<div id="edgeRight" class="displayEdges" style="display: none"><div id="stepNext"></div></div>
+
+		<div id="lightbox" style="display: none"></div>
 		
 		<script type="text/javascript" src="js/jquery-2.0.3.min.js"></script>
 		<script type="text/javascript" src="js/master.js"></script>
